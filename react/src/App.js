09559df90a9a0a9972ec2cd9675fb6d29fcd90c3
Tan Phat <56815516+NgoTanPhat-CDTH17PMC@users.noreply.github.com>
2023-08-18 14:52:21 +0700

    useSelector, useDispatch

--- react/src/App.js
+++ react/src/App.js
@@ -1,36 +1,53 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { increaseCounter, decreaseCounter } from "./action/actions";
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
+import store from "./redux/store";
 
 function App(props) {
+  const dispatch = useDispatch();
+  const newCount = useSelector((state) => {
+    return state.counter.count;
+  });
+
+  const handleIncrease = () => {
+    dispatch(increaseCounter());
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and Hoi Dan IT!</h1>
-        <div>Count: {props.count}</div>
+        <div>Count: {newCount}</div>
 
-        <button onClick={() => props.increaseCounter()}>Increase Count</button>
+        <button onClick={() => handleIncrease()}>Increase Count</button>
 
-        <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+        <button onClick={() => dispatch(decreaseCounter())}>
+          Decrease Count
+        </button>
       </header>
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    count: state.counter.count,
-  };
-};
+// // mapping  State của Redux Store vào Props của React
+// const mapStateToProps = (state) => {
+//   return {
+//     // state.count là count được khai báo từ file reducer
+//     count: state.counter.count,
+//   };
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
+// // mapping Dispatch (hàm) của Redux vào Props React
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increaseCounter: () => dispatch(increaseCounter()),
 
-    decreaseCounter: () => dispatch(decreaseCounter()),
-  };
-};
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default App;
--- react/src/redux/store.js
+++ react/src/redux/store.js
@@ -2,9 +2,10 @@ import { createStore } from "redux";
 import rootReducer from "../reducer/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+// bao gom: reducer, middelware...
 const store = createStore(
   rootReducer,
-  composeWithDevTools() // dung cho redux fund
+  composeWithDevTools() // dung cho redux tund, saga
 );
 
 export default store;
