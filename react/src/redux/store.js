4aa37f586930fb3ba80b9c365a4efd5732e6fd33
Tan Phat <56815516+NgoTanPhat-CDTH17PMC@users.noreply.github.com>
2023-08-21 22:18:52 +0700

    setup redux thunk - xu ly redux fetching data

--- react/src/redux/store.js
+++ react/src/redux/store.js
@@ -1,11 +1,12 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducer/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
 
 // bao gom: reducer, middelware...
 const store = createStore(
   rootReducer,
-  composeWithDevTools() // dung cho redux tund, saga
+  composeWithDevTools(applyMiddleware(thunkMiddleware)) // dung cho redux tund, saga
 );
 
 export default store;
