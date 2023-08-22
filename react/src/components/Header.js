a138cf94a2fa4c53ed3fc5f3616e49ed2122d441
Tan Phat <56815516+NgoTanPhat-CDTH17PMC@users.noreply.github.com>
2023-08-22 21:50:50 +0700

    count user - chia se data redux

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -2,8 +2,10 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { useSelector } from "react-redux";
 
 const Header = (props) => {
+  const listUsers = useSelector((state) => state.user.listUsers);
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="bg-body-tertiary">
       <Container>
@@ -13,16 +15,20 @@ const Header = (props) => {
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            <NavDropdown
+              className=""
+              title={`Users (${listUsers.length})`}
+              id="basic-nav-dropdown"
+            >
+              {listUsers &&
+                listUsers.length > 0 &&
+                listUsers.map((item, index) => {
+                  return (
+                    <NavDropdown.Item key={`user-${index + 1}`} href="#">
+                      {item.username}-{item.email}
+                    </NavDropdown.Item>
+                  );
+                })}
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
